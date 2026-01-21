695fec301dec2ab05fda8e39e0ced43049b43a7e
Elias <elias.gs22@gmail.com>
2026-01-20 23:29:29 -0300

    Agregar timeout a geolocalizacion

--- web/script.js
+++ web/script.js
@@ -71,7 +71,7 @@ if (action === 'register') {
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error);
+        navigator.geolocation.getCurrentPosition(success, error, {timeout: 5000});
     } else {
         alert('Geolocalización no soportada por este navegador.');
     }
